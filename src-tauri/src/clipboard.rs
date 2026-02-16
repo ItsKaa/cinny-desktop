4be4c42fd081ed9d32a1cdca0a6e764fd467bb4e
ItsKaa <31796925+ItsKaa@users.noreply.github.com>
2026-02-16 21:07:27 +0100

    use webp from clipboard

--- src-tauri/src/clipboard.rs
+++ src-tauri/src/clipboard.rs
@@ -1,14 +1,13 @@
 use arboard::{Clipboard};
 use base64::{engine::general_purpose, Engine as _};
-use image::{ImageBuffer, Rgba, codecs::png::PngEncoder};
+use image::{DynamicImage, ImageBuffer, Rgba};
 use std::io::Cursor;
-use tauri;
 
 #[tauri::command]
 pub fn clipboard_read_image() -> Result<String, String> {
     let mut clipboard = Clipboard::new().map_err(|e| e.to_string())?;
     let image_data = clipboard.get_image().map_err(|e| e.to_string())?;
-    
+
     let mut img: ImageBuffer<Rgba<u8>, Vec<u8>> = ImageBuffer::from_raw(
         image_data.width.try_into().map_err(|_| "Invalid width".to_string())?,
         image_data.height.try_into().map_err(|_| "Invalid height".to_string())?,
@@ -31,13 +30,13 @@ pub fn clipboard_read_image() -> Result<String, String> {
         }
     }
 
-    let mut png_bytes = Vec::new();
-    let mut cursor = Cursor::new(&mut png_bytes);
-    let encoder = PngEncoder::new(&mut cursor);
-    img.write_with_encoder(encoder)
-        .map_err(|e| format!("Failed to encode PNG: {}", e))?;
+    // Convert to WEBP with fixed quality percentage.
+    let dyn_img = DynamicImage::ImageRgba8(img);
+    let encoder = webp::Encoder::from_image(&dyn_img)
+        .map_err(|e| format!("Failed to create a webp encoder: {}", e))?;
+    let mut webp_data = encoder.encode(95.0);
 
-    let base64_str = general_purpose::STANDARD_NO_PAD.encode(png_bytes);
+    let base64_str = general_purpose::STANDARD_NO_PAD.encode(&webp_data[..]);
 
     Ok(base64_str)
 }
